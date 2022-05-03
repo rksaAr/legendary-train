b3cdea1b8f8d345f9c2950f4f1a446c5a3e3e983
Ronak Sutariya <ronak.sutariya@artoon.in>
2022-05-03 16:39:32 +0530

    remove extra code

--- src/app.js
+++ src/app.js
@@ -14,36 +14,4 @@ app.use(express.urlencoded({ extended: false }))
 app.use(require('./routes/tasks.routes'));
 //Initialize
 
-//Settings
-app.set('port', process.env.PORT || 3000);
-
-//Middlewares
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-
-//Routes
-app.use(require('./routes/tasks.routes'));
-//Initialize
-
-//Settings
-app.set('port', process.env.PORT || 3000);
-
-//Middlewares
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-
-//Routes
-app.use(require('./routes/tasks.routes'));
-//Initialize
-
-//Settings
-app.set('port', process.env.PORT || 3000);
-
-//Middlewares
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-
-//Routes
-app.use(require('./routes/tasks.routes'));
-
 module.exports = app;
