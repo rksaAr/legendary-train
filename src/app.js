d3445cf63624209914f9ddd7f82fa5950e85c409
Ronak Sutariya <ronak.sutariya@artoon.in>
2022-05-03 16:37:58 +0530

    add Extra OCde

--- src/app.js
+++ src/app.js
@@ -1,14 +1,47 @@
 const express = require('express');
+const app = express();
 
 //Initialize
-const app = express();
 
 //Settings
 app.set('port', process.env.PORT || 3000);
 
 //Middlewares
 app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
+
+//Routes
+app.use(require('./routes/tasks.routes'));
+//Initialize
+
+//Settings
+app.set('port', process.env.PORT || 3000);
+
+//Middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+//Routes
+app.use(require('./routes/tasks.routes'));
+//Initialize
+
+//Settings
+app.set('port', process.env.PORT || 3000);
+
+//Middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+//Routes
+app.use(require('./routes/tasks.routes'));
+//Initialize
+
+//Settings
+app.set('port', process.env.PORT || 3000);
+
+//Middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 //Routes
 app.use(require('./routes/tasks.routes'));
